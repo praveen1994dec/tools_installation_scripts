d33b3eb4de119141e4dbc9030b4bb563530e3e0f
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-06-02 19:26:55 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update -y
+
+sudo apt upgrade -y 
+
+sudo apt install openjdk-17-jre -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
