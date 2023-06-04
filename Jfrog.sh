eb27fe57d018a5a9c16606e6a7b1e0d413be583e
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-06-04 09:01:46 +0530

    Update Jfrog.sh

--- Jfrog.sh
+++ Jfrog.sh
@@ -1,4 +1,8 @@
 docker pull docker.bintray.io/jfrog/artifactory-oss:latest
 sudo mkdir -p /jfrog/artifactory
 sudo chown -R 1030 /jfrog/
-docker run --name artifactory -d -p 8081:8081  -p 8082:8082 -v /jfrog/artifactory:/var/opt/jfrog/artifactory docker.bintray.io/jfrog/artifactory-oss:latest
+docker run --name artifactory -d \
+  -p 8081:8081 \
+  -p 8082:8082 \
+  -v /jfrog/artifactory:/var/opt/jfrog/artifactory \
+  docker.bintray.io/jfrog/artifactory-oss:latest
