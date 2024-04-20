3f49ace91cc46147b3ef1a675c42ab6b62b5f0d1
Praveen Singampalli <praveensingampalli1994@gmail.com>
2024-04-20 12:27:17 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#-f Fail silently (no output at all) on server errors.
+#s--silent Silent or quiet mode. Don't show progress meter or error messages.
+#-S, --show-error and when used with -s , it will show an error
+#-L, --location If the server reports that the requested page has moved to a different location
+#A System/software update is typically a release containing enhancements to the current version. 
+#An upgrade is a whole new version of software that represents a significant change or major improvement.
+#DEB - A deb package (.deb file) is a software package in a specific format designed for Debian-based distributions recognized by the .deb extension. \
+#Deb packages allow installing local software on an Ubuntu system.
+
+
+
+
+
 sudo apt update -y
 
 sudo apt upgrade -y 
